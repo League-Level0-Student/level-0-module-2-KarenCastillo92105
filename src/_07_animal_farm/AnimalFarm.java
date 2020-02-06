fda961fa597c1b79bf8cc174be4520b134a9887d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-02-06 13:25:12 -0800

    AnimalFarm

--- src/_07_animal_farm/AnimalFarm.java
+++ src/_07_animal_farm/AnimalFarm.java
@@ -14,14 +14,37 @@ import javax.swing.JOptionPane;
 
 public class AnimalFarm {
 	
+	
+
 	public static void main(String[] args) {
-			
+		
 		/* 1. Ask the user which animal they want, then play the sound of that animal. */			 
-			
+		String animal=JOptionPane.showInputDialog("What animal do you want");
 		/* 2. Make it so that the user can keep entering new animals. */
+if(animal.equalsIgnoreCase("cow")){
+	playMoo();
+}
+else if(animal.equalsIgnoreCase("duck")) {
+	playQuack();
+}
+else if(animal.equalsIgnoreCase("dog")) {
+	playWoof();
+	}
+else if(animal.equalsIgnoreCase("cat")) {
+     playMeow();
+}
+else if(animal.equalsIgnoreCase("llama")) {
+	playLlama();
+}
+else{
+	JOptionPane.showMessageDialog(null,"sorry i don't know that animal");
+}
+
 
 	}
 
+
+	
 	static void playMoo() {
 		playNoise(mooFile);
 	}
