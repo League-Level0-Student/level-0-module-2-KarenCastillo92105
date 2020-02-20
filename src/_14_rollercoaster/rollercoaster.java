248f4f6f74de98f4e6b80f87f5ed1706f92537a1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-02-20 13:29:16 -0800

    parseInt

---
+++ src/_14_rollercoaster/rollercoaster.java
@@ -0,0 +1,23 @@
+package _14_rollercoaster;
+
+import javax.swing.JOptionPane;
+
+public class rollercoaster {
+public static void main(String[] args) {
+
+
+String height=JOptionPane.showInputDialog("What is your height?");
+int num=Integer.parseInt(height);
+if (num>48) {
+	
+	
+	
+	JOptionPane.showMessageDialog(null, "Nice your good to go!");}
+else {
+JOptionPane.showMessageDialog(null, "No your not q");
+	
+	
+	
+}
+}
+}
